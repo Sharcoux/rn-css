ff0c3858265ee310abb78994dc0bd38250de965e
François BILLIOUD <f.billioud@gmail.com>
2020-06-02 23:07:32 +0200

    Fix attrs for Lists
    Signed-off-by: François BILLIOUD <f.billioud@gmail.com>

--- index.tsx
+++ index.tsx
@@ -50,7 +50,11 @@ const ColorCircle = styled.TouchableOpacity<{color: string; size?: number}>`
   backgroundColor: ${props => props.color};
   width: ${props => props.size || 2}em;
   height: ${props => props.size || 2}em;
+  opacity: 1;
   border-radius: 50%;
+  &:hover {
+    opacity: 0.5;
+  }
 `
 
 const FlatList = () => {
